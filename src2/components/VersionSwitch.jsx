966dcbdb7ba1e6f9323aaf0ad25120fade91a2df
cinkovic <cin.qqq@gmail.com>
2025-02-02 19:00:13 +0100

    pre-deployment

--- src2/components/VersionSwitch.jsx
+++ src2/components/VersionSwitch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function VersionSwitch() {
   const isBeta = window.location.pathname.includes('beta');
-  const href = isBeta ? '/' : '/beta';
+  const href = isBeta ? '/' : '/index_beta.html';
   
   return (
     <a href={href} id="bottomButton" aria-label={`Switch to ${isBeta ? 'simple' : 'beta'} version`}>
